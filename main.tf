90005e280e2d04fde621389c680bacd2403662a2
Jorge Turrado <jorge_turrado@hotmail.es>
2022-10-12 19:39:24 +0200

    dummy change
    Signed-off-by: Jorge Turrado <jorge_turrado@hotmail.es>

--- main.tf
+++ main.tf
@@ -38,7 +38,11 @@ module "github_secrets" {
     },
     {
       name  = "TF_AZURE_STORAGE_CONNECTION_STRING"
-        value = module.azure_storage_account.storage_account_connectionstring
+      value = module.azure_storage_account.storage_account_connectionstring
+    },
+    {
+      name  = "TF_AZURE_STORAGE_CONNECTION_STRING_2"
+      value = module.azure_storage_account.storage_account_connectionstring
     },
   ]
 }
