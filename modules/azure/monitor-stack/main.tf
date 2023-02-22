fe8f13827c9bb02feb292c6b951cb648bf02fadc
Jorge Turrado <jorge.turrado@scrm.lidl>
2023-02-22 12:09:06 +0100

    add prometheus rule
    Signed-off-by: Jorge Turrado <jorge.turrado@scrm.lidl>

--- modules/azure/monitor-stack/main.tf
+++ modules/azure/monitor-stack/main.tf
@@ -74,7 +74,7 @@ resource "azurerm_resource_group_template_deployment" "azure_monitor_workspace" 
     ],
     "outputs": {
       "workspace_id": {
-        "type": "string",
+        "type": "String",
         "value": "[resourceId('microsoft.monitor/accounts', parameters('workspace_name'))]"
       }
     }
