8324f956954072425ebd9560615395754700aed6
Jorge Turrado <jorge_turrado@hotmail.es>
2023-08-26 14:09:32 +0200

    bump modules
    Signed-off-by: Jorge Turrado <jorge_turrado@hotmail.es>

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -6,26 +6,30 @@ terraform {
   required_version = ">= 1.3.0"
 
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.36.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "=5.14.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "=2.29.0"
+      version = "=2.41.0"
     }
-    aws = {
-      source  = "hashicorp/aws"
-      version = "=4.35"
-    }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "=4.0.3"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.71.0"
     }
     google = {
       source  = "hashicorp/google"
-      version = "=4.44.0"
+      version = "=4.79.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "=3.5.1"
+    } 
+    tls = {
+      source  = "hashicorp/tls"
+      version = "=4.0.4"
+    }    
   }
 }
 
