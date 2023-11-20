7552bff249d62bb8393b52ceecc080e91b6cb858
Jorge Turrado Ferrero <Jorge_turrado@hotmail.es>
2023-11-21 00:29:33 +0100

    Update main.tf
    Signed-off-by: Jorge Turrado Ferrero <Jorge_turrado@hotmail.es>

--- terraform/modules/azure/data-explorer/main.tf
+++ terraform/modules/azure/data-explorer/main.tf
@@ -17,6 +17,7 @@ resource "azurerm_kusto_cluster" "cluster" {
   name                = local.kusto_cluster_name
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
+  auto_stop_enabled   = false
 
   sku {
     name     = "Dev(No SLA)_Standard_E2a_v4"
