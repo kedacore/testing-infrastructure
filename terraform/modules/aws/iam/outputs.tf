80710f224f72fe9ad4225e2f60a32ac7f9d283a3
Jorge Turrado <jorge.turrado@scrm.lidl>
2023-12-30 20:29:56 +0100

    Add a second workload role for AWS assume role e2e test
    Signed-off-by: Jorge Turrado <jorge.turrado@scrm.lidl>

--- terraform/modules/aws/iam/outputs.tf
+++ terraform/modules/aws/iam/outputs.tf
@@ -6,6 +6,6 @@ output "e2e_user_secret_key" {
   value = aws_iam_access_key.e2e_test.secret
 }
 
-output "workload_role_arn" {
-  value = aws_iam_role.workload_role.arn
+output "workload1_role_arn" {
+  value = aws_iam_role.workload1_role.arn
 }
