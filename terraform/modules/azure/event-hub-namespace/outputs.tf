d082372f9939b20f1bdb1c68ff8fe1e811b5b46c
Jorge Turrado <jorge_turrado@hotmail.es>
2024-02-02 16:47:28 +0100

    chore: Add TF_AZURE_EVENTHUB_NAMESPACE secret
    Signed-off-by: Jorge Turrado <jorge_turrado@hotmail.es>

--- terraform/modules/azure/event-hub-namespace/outputs.tf
+++ terraform/modules/azure/event-hub-namespace/outputs.tf
@@ -1,3 +1,7 @@
 output "manage_connection_string" {
   value = azurerm_eventhub_namespace_authorization_rule.manage_connection.primary_connection_string
 }
+
+output "namespace_name" {
+  value = local.event_hub_name
+}
