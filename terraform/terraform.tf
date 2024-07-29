98f8e06814452f50d7359fe4e42bea707720b3c4
Jorge Turrado <jorge.turrado@scrm.lidl>
2024-07-29 21:14:13 +0200

    chore: Bump providers
    Signed-off-by: Jorge Turrado <jorge.turrado@scrm.lidl>

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -8,27 +8,27 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "=5.14.0"
+      version = "=5.60.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "=2.41.0"
+      version = "=2.53.1"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.71.0"
+      version = "=3.113.0"
     }
     google = {
       source  = "hashicorp/google"
-      version = "=4.79.0"
+      version = "=5.39.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "=3.5.1"
+      version = "=3.6.2"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = "=4.0.4"
+      version = "=4.0.5"
     }
   }
 }
