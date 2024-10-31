c1cf0677cdbe4ea8fb9e5ebbc2c58a8adffde4e5
Jorge Turrado Ferrero <Jorge_turrado@hotmail.es>
2024-10-31 18:37:21 +0100

    feat: Remove AAD PodIdentity resources (#162)
    Signed-off-by: Jorge Turrado <jorge_turrado@hotmail.es>

--- terraform/modules/azure/aks/main.tf
+++ terraform/modules/azure/aks/main.tf
@@ -66,27 +66,6 @@ resource "azurerm_federated_identity_credential" "msi_federation" {
   subject             = "system:serviceaccount:keda:keda-operator"
 }
 
-## AAD-Pod-Identity role assignements
-
-data "azurerm_resource_group" "aks_nodes" {
-  name = azurerm_kubernetes_cluster.aks.node_resource_group
-  depends_on = [
-    azurerm_kubernetes_cluster.aks,
-  ]
-}
-
-resource "azurerm_role_assignment" "kubelet_virtual_machine_contributor" {
-  scope                = data.azurerm_resource_group.aks_nodes.id
-  role_definition_name = "Virtual Machine Contributor"
-  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
-}
-
-resource "azurerm_role_assignment" "kubelet_identity_operator" {
-  scope                = data.azurerm_resource_group.rg.id
-  role_definition_name = "Managed Identity Operator"
-  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
-}
-
 ## Azure Managed prometheus
 
 resource "azurerm_resource_group_template_deployment" "dce" {
