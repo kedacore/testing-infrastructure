9e2a719297b1c25b4f5754a78d74ba16f6429e65
Jorge Turrado <jorge_turrado@hotmail.es>
2024-11-01 01:27:47 +0100

    Update the cluster to update the nodes
    Signed-off-by: Jorge Turrado <jorge_turrado@hotmail.es>

--- terraform/modules/azure/aks/main.tf
+++ terraform/modules/azure/aks/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    kubectl = {
+      source = "gavinbunney/kubectl"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
   skip_provider_registration = true
