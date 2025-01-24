e74f9949830a240f86739ab3bc58b1feb6bf468f
Jorge Turrado <jorge_turrado@hotmail.es>
2025-01-24 23:06:23 +0100

    wip
    Signed-off-by: Jorge Turrado <jorge_turrado@hotmail.es>

--- terraform/modules/grafana/iam/main.tf
+++ terraform/modules/grafana/iam/main.tf
@@ -2,7 +2,6 @@ terraform {
   required_providers {
     grafana = {
       source  = "grafana/grafana"
-      version = "2.2.0"
     }
   }
 }
--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,42 +1,46 @@
 terraform {
-  backend "azurerm" {
-    key = "keda.tfstate"
-  }
+  # backend "azurerm" {
+  #   key = "keda.tfstate"
+  # }
 
-  required_version = ">= 1.5.6"
+  required_version = ">= 1.10.5"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "=5.60.0"
+      version = "=5.84.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "=2.53.1"
+      version = "=3.1.0"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.113.0"
+      version = "=4.16.0"
     }
     google = {
       source  = "hashicorp/google"
-      version = "=5.39.0"
+      version = "=6.17.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "=3.6.2"
+      version = "=3.6.3"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = "=4.0.5"
+      version = "=4.0.6"
     }
     kubectl = {
       source  = "alekc/kubectl"
-      version = "2.1.2"
+      version = "2.1.3"
     }
     mssql = {
       source  = "betr-io/mssql"
       version = "0.3.1"
+    }
+    grafana = {
+      source  = "grafana/grafana"
+      version = "3.17.1"
     }
   }
 }
